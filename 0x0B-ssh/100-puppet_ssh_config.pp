a0aae09b473b49aeb915b35ab7fd7c7969c86101
camia97 <3962@holbertonschool.com>
2022-04-19 15:28:41 +0000

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Client configuration file (w/ Puppet)
 file { '/etc/ssh/ssh_config':
-  path    => '/etc/ssh/ssh_config'
+  path    => '/etc/ssh/ssh_config',
   owner   => 'root',
   group   => 'root',
   mode    => '0744',
