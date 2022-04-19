2e2dcc58e0843272207325ba558f8b54b3b74def
camia97 <3962@holbertonschool.com>
2022-04-19 15:30:01 +0000

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,8 @@
 # Client configuration file (w/ Puppet)
 file { '/etc/ssh/ssh_config':
-  path    => '/etc/ssh/ssh_config',
+  ensure  => 'present',
   owner   => 'root',
   group   => 'root',
   mode    => '0744',
-  content => 'Host * \nPasswordAuthentication no\n IdentityFile ~/.ssh/school'
+  content => 'Host * \n PasswordAuthentication no\n IdentityFile ~/.ssh/school'
 }
